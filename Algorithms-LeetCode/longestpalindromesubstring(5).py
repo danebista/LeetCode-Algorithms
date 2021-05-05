3e7d448d102eeaeba9a3837fbd242bf8a0749ba0
Your Name <bista.dinank@gmail.com>
2021-05-05 16:46:01 +0545

    Algo 5: Longest Palindrome Substring

---
+++ Algorithms-LeetCode/longestpalindromesubstring(5).py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res=""
+        resL=0
+        for i in range(len(s)):
+            l=i
+            r=i
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                if (r-l+1)>resL:
+                    resL=r-l+1
+                    res=s[l:r+1]
+                l-=1
+                r+=1
+         
+            l=i
+            r=i+1
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                if (r-l+1)>resL:
+                    resL=r-l+1
+                    res=s[l:r+1]
+                l-=1
+                r+=1
+            
+        return res
+            
+            
+         
