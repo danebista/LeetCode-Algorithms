0ac916e793262c5c5f55c0a4863d6fd49ba4c1df
Your Name <bista.dinank@gmail.com>
2021-05-16 22:18:18 +0545

    Improved code for Find Median of Sorted Arrays

--- Algorithms-LeetCode/1-10/4. MedianSortedArrays.py
+++ Algorithms-LeetCode/1-10/4. MedianSortedArrays.py
@@ -21,44 +21,28 @@ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        median1, median2= -1,-1
+         median1, median2 = -1,-1
+        m, n =  len(nums1), len(nums2)
+        maxlength = m + n
         l,r=0,0
-        m,n=len(nums1), len(nums2)
-        maxlength= m + n
-        
-        if (maxlength % 2 == 1):
-            for i in range(maxlength//2 + 1):
-                if (l != m and r != n):
-                    if nums1[l] >nums2[r]:
-                        median1= nums2[r]
-                        r+=1
-                    else:
-                        median1=nums1[l]
-                        l+=1
-                    
-                elif l<m:
-                    median1=nums1[l]
-                    l+=1
-                else:
-                    median1=nums2[r]
-                    r+=1
-            return median1
-        
         for i in range(maxlength//2 + 1):
             median2=median1
-            if (l != m and r != n):
-                if nums1[l] >nums2[r]:
-                    median1= nums2[r]
-                    r+=1
-                else:
-                    median1=nums1[l]
+            if (l<m and r<n):
+                if nums1[l]<=nums2[r]:
+                    median1= nums1[l]
                     l+=1
-                    
-            elif l<m:
+                else:
+                    median1 = nums2[r]
+                    r+=1
+            elif (l<m):
                 median1=nums1[l]
                 l+=1
             else:
                 median1=nums2[r]
                 r+=1
+            
+        if (m+n) % 2 ==1 :
+            return median1
+        else:
+            return (median1+median2)/2
         
-        return (median1 + median2)/2
