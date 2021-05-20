4bbd4592e6822110e5bbfb42b68096790367b8a4
Your Name <bista.dinank@gmail.com>
2021-05-20 21:19:02 +0545

    Improved code for valid parenthesis

--- Algorithms-LeetCode/11-20/20. Valid Parenthesis.py
+++ Algorithms-LeetCode/11-20/20. Valid Parenthesis.py
@@ -2,26 +2,23 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
         if len(s)==0: return False
-  
-        res=[]
+        
+        res = []
+        
         for i in s:
             
-            if i ==")" and len(res)>0 and res[len(res)-1]=="(":
-                
-                res.pop()
-                continue
-                    
-            elif i=="]" and len(res)>0 and res[len(res)-1]=="[":
-                res.pop()
-                continue
-            
-            elif i=="}" and len(res)>0 and res[len(res)-1]=="{":
-                res.pop()
-                continue
-            
-            elif i =="(" or i=="[" or i=="{":
+            if i=='(' or i=='{' or i=='[':
                 res.append(i)
-                continue
-            return False
-        
+                
+            elif i==')' and len(res)>0 and res[len(res)-1]=='(':
+                res.pop()
+            
+            elif i=='}' and len(res)>0 and res[len(res)-1]=='{':
+                res.pop()
+            
+            elif i==']' and len(res)>0 and res[len(res)-1]=='[':
+                res.pop()
+            else:
+                return False
+            
         return len(res)==0
