f6582f973bd2cc054aa3fcc4bf5305ff54218d22
Your Name <bista.dinank@gmail.com>
2021-05-22 18:00:56 +0545

    String Multiply

--- Algorithms-LeetCode/41-50/43. multiplystrings.py
+++ Algorithms-LeetCode/41-50/43. multiplystrings.py
@@ -1,17 +1,23 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(self.stringToIntegerConverter(num1) * self.stringToIntegerConverter(num2))
-    
-    
-    def stringToIntegerConverter(self, num):
-        result=0
-        
-        for digit in num:
-            result *= 10
        
-            for d in '0123456789':
+        output=[]
+        is_zero_leading = True
+        a= [0] * (len(num1)+len(num2))
+        output=''
+        for i in range(len(num1)-1, -1,-1):
+            for j in range(len(num2)-1,-1,-1):
+                
+                value=int(num1[i]) * int(num2[j])+ a[i+j+1]
+                
+                a[i+j] += value //10
+                a[i+j+1] = value % 10
+        for i in range(len(a)):
+            if a[i] !=0:
+                is_zero_leading = False
             
-                result += digit > d
+            if is_zero_leading == False:
+                output+=str(a[i])
+        if output=="": return "0"
         
-
-        return result
+        return output
