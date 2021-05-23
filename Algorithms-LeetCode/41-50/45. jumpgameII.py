3bcab945c3dbae5d9294830eb9289182cdcd861b
Your Name <bista.dinank@gmail.com>
2021-05-24 02:28:38 +0545

    45. and 46. Permutation and Jump Games

---
+++ Algorithms-LeetCode/41-50/45. jumpgameII.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        jumpsMade = 1
+        
+        if n == 1 or nums[0] == 0:
+        
+            return 0
+        
+        farthest = nums[0]
+        current_end = nums[0]
+        
+        for i in range(1, n):
+            print('i: {}'.format(i))
+            if i == n-1:
+                return jumpsMade
+            
+            if i == current_end:
+                jumpsMade += 1
+                current_end = farthest
+        
+        return jumpsMade
