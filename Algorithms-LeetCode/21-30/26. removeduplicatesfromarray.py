4fe23f376859ecd9d6f9327e5f494e9aa8181798
Your Name <bista.dinank@gmail.com>
2021-06-01 22:50:33 +0545

    Improved logic for number 2

--- Algorithms-LeetCode/21-30/26. removeduplicatesfromarray.py
+++ Algorithms-LeetCode/21-30/26. removeduplicatesfromarray.py
@@ -1,18 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        r=0
         n=len(nums)
+        
         if n<=1: return n
         
-        l,r=1,1
+        r=1
         
-        while(l<n):
-            if nums[l]==nums[l-1]:
-                l+=1
-            else:
+        for l in range(1,n):
+            if nums[l] != nums[l-1]:
                 nums[r]=nums[l]
                 r+=1
-                l+=1
         
         return r
-        
