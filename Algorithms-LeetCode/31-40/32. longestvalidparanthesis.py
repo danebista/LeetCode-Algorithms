328f7e4a9db3922aae5216a85f11a4aa44d6c628
Your Name <bista.dinank@gmail.com>
2021-06-04 21:31:00 +0545

    Added algo to improve longest valid parenthesis

--- Algorithms-LeetCode/31-40/32. longestvalidparanthesis.py
+++ Algorithms-LeetCode/31-40/32. longestvalidparanthesis.py
@@ -2,22 +2,20 @@ class Solution:
     def longestValidParentheses(self, s: str) -> int:
         res=[0 for x in s]
         if len(res)==0: return 0
-        
-        for i in range(len(s)):
+      
+        for i in range(1, len(s)):
             
             if s[i]==')':
-                if i-1<0: 
-                    continue
-                
+            
                 if s[i-1]=='(':
-                    res[i]=  res[i-2]+2
+                
+                    res[i]=res[i-2]+2
                     continue
                 
                 if i-res[i-1]-1<0:
                     continue
                 
-                if s[i-res[i-1]-1]=='(':
-                    res[i]=res[i-1]+ 2+ res[i-res[i-1]-2]
-                
-        
+                if s[i-res[i-1]-1]=="(":
+                    res[i]=res[i-1]+2+res[i-res[i-1]-2]
+            
         return max(res)
