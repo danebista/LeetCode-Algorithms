eaadd60f319cbe65eee56c6650d0044ca363def9
Your Name <bista.dinank@gmail.com>
2021-06-08 23:04:08 +0545

    Text justification added

---
+++ Algorithms-LeetCode/61-70/68. textjustification.py
@@ -0,0 +1,31 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        if not words:
+            return
+        
+        result = []
+        curr = []
+        num_of_letters = 0
+        
+        for w in words:
+            if len(w) + num_of_letters + len(curr)> maxWidth:
+                if len(curr) == 1:
+                    result.append(curr[0] + ' '* (maxWidth - num_of_letters))
+                
+                else:
+                    num_of_spaces = maxWidth - num_of_letters
+                    space, extra = divmod(num_of_spaces, len(curr)-1)
+                    
+                    for i in range(extra):
+                        curr[i] +=' '
+                    
+                    result.append((' '*space).join(curr))
+                curr=[]
+                num_of_letters=0
+            
+            curr.append(w)
+            num_of_letters += len(w)
+        
+        result.append(' '.join(curr)+ ' ' * (maxWidth-num_of_letters-len(curr)+1))
+                
+        return result
