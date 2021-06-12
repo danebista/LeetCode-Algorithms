12b7ca56038afe84035d5f53736306cefd98f6e6
Your Name <bista.dinank@gmail.com>
2021-06-12 23:24:51 +0545

    sudukosolver

--- Algorithms-LeetCode/31-40/37. sudukosolver.py
+++ Algorithms-LeetCode/31-40/37. sudukosolver.py
@@ -1,44 +1,47 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        return self.solve(board)
-       
-    def solve(self, board):
-        position = self.findEmpty(board)
-        if not position :
-            return True
-        
-        
-        for i in range(1,10):
-            if self.validBoardSquare(board, i, position):
-                board[position[0]][position[1]]=str(i)
-                
-                if (self.solve(board)):
-                    return True
-                board[position[0]][position[1]]='.'  
-        return False    
-    
-    def findEmpty(self, board):
-        for i in range(9):
-            for j in range(9):
-                if board[i][j]=='.':
-                    return (i,j)
-                
+        self.solver(board)
         return None
     
-    def validBoardSquare(self, board,num, pos):
+        """
+        Do not return anything, modify board in-place instead.
+        """
+    
+    def solver(self,board):
+        position =self.findEmptySquare(board)
         
+        if not position:
+            return True
+        
+        for i in range(1,10):
+            validSquare = self.checkValidSquare(board, position, i)
+            if validSquare:
+                board[position[0]][position[1]] = str(i)
+                
+                if self.solver(board) == True:
+                    return True
+                board[position[0]][position[1]] = '.'
+        
+        return False
+        
+    def checkValidSquare(self, board, pos, num):
         for i in range(9):
-            if board[pos[0]][i] != '.' and int(board[pos[0]][i]) == num and i!=pos[1]:
+            if board[pos[0]][i] != '.' and i!=pos[1] and board[pos[0]][i] == str(num):
                 return False
         for j in range(9):
-            if board[j][pos[1]] != '.' and int(board[j][pos[1]])==num and j!=pos[0]:
+            if board[j][pos[1]] != '.' and j != pos[0] and board[j][pos[1]] == str(num):
                 return False
+        box_x= pos[0]//3
+        box_y= pos[1]//3
         
-        box_x=pos[0]//3
-        box_y=pos[1]//3
-        for i in range(box_x * 3, box_x*3+3):
-            for j in range(box_y *3, box_y*3+3):
-                if board[i][j] != '.' and int(board[i][j])==num and (i,j)!=pos:
+        for i in range(box_x *3, box_x*3+3):
+            for j in range(box_y*3, box_y*3+3):
+                if board[i][j] != '.' and pos != (i,j) and board[i][j] == str(num):
                     return False
-        
         return True
+    def findEmptySquare(self,board):
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    return (i,j)
+        return False
