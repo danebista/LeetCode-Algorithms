f27de60904b182a3b2cb6b8166bb2b2569d1c379
Your Name <bista.dinank@gmail.com>
2021-06-13 10:43:53 +0545

    unnecessary comparison removed

--- Algorithms-LeetCode/31-40/38. countandsay.py
+++ Algorithms-LeetCode/31-40/38. countandsay.py
@@ -4,15 +4,15 @@ class Solution:
         if n==1:
             return "1"
         
-        previousIterator = self.countAndSay(n-1)
-        count=1
-        num=len(previousIterator)
-        for i in range(num):
-            if i==num-1 or (i+1< num and previousIterator[i]!=previousIterator[i+1]):
-                res+=str(count)+str(previousIterator[i])
-                count=1
-            else:
-                count+=1
+        previousValue = self.countAndSay(n-1)
+        count = 1
+        len_nums = len(previousValue)
         
+        for i in range(len_nums):
+            if i == len_nums - 1 or (i+1 < len_nums and previousValue[i] != previousValue[i+1]):
+                res += str(count) + previousValue[i]
+                count = 1
+            else:
+                count += 1
         return res
             
